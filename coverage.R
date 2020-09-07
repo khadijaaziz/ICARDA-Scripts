da9c1d75f8295bb5aa015cb442733996a91d88ed
Khadija AZIZ <49232925+khadijaaziz@users.noreply.github.com>
2020-09-07 10:16:39 +0100

    improve coverage function

--- coverage.R
+++ coverage.R
@@ -9,55 +9,62 @@ sdm_model<- raster("./durum_sdm_all.tif")
 
 trait_rast <- raster("./ST.S.Raster.tif")
 
-#load data
-load(".")
-
+# Load RData
+load("C:/Users/yasse_000/Desktop/Coverage/RData/ScubOutput.RData")
+data <- '' #put the loaded data in this value
 # Define the threshold
-lower <- mean(ST.S.output$Prediction) + sd(ST.S.output$Prediction)
-upper <- mean(ST.S.output$Prediction) 
+xmin <- extent(sdm_model)[1]
+xmax <- extent(sdm_model)[2]
+ymin <- extent(sdm_model)[3]
+ymax <- extent(sdm_model)[4]
 
+data <- data[data$Longitude <= xmax & data$Longitude >= xmin ,]
+data <- data[data$Latitude <= ymax & data$Latitude >= ymin ,]
+
+lower <- mean(data$Prediction) + sd(data$Prediction)
+upper <- mean(data$Prediction) 
 
 # Preparing rasters
 trait_rast<- resample(trait_rast,wmask,method='ngb')
-plot(trait_rast)
 
-# sumRas <- summary(trait_rast)
-# sdRas <- calc(trait_rast, sd)
 sdm_model<- resample(sdm_model,wmask,method='ngb')
 
-# Calculate the coverage Coverage
+
+# Calculate the SDM area
 rast_area <- raster::area(wmask) * wmask
 sdm_mask <- raster::mask(wmask, sdm_model)
-
 sdm_area <- sdm_mask * rast_area
 total_sdm_area <- sum(sdm_area[], na.rm = TRUE)
 
 
 #Calculate the High Confidence coverage
-
 high_conf <- trait_rast
 high_conf[which(high_conf[] >= upper)] <- 1
 high_conf[which(high_conf[] < upper)] <- NA
 high_conf <- high_conf * rast_area
+high_conf <- raster::mask(high_conf, mask = sdm_mask)
 total_hg_conf <- sum(high_conf[], na.rm = TRUE)
 
-high_conf <-  (total_hg_conf/total_sdm_area)
+high_conf_percent <-  (total_hg_conf/total_sdm_area)*100
 
-rm(high_conf);g <- gc(); rm(g)
+g <- gc(); rm(g)
 
 
 #Calculate the Low Confidence coverage 
-
 low_conf <- trait_rast
 low_conf[which(low_conf[] >= lower)] <- 1
 low_conf[which(low_conf[] <lower)] <- NA
 low_conf <- low_conf * rast_area
+low_conf <- raster::mask(low_conf, mask = sdm_mask)
 total_lw_conf <- sum(low_conf[], na.rm = TRUE)
 
-low_conf <-  (total_lw_conf/total_sdm_area)
+low_conf_percent <-  (total_lw_conf/total_sdm_area)*100
 
-rm( rast_area, sdm_mask );g <- gc(); rm(g)
+g <- gc(); rm(g)
 
-#Print the results
-cat("High Confidence coverage: ",high_conf, sep = '')
-cat("Low Confidence coverage: ",low_conf, sep = '')
+cat("High Confidence coverage: ",high_conf_percent,'%', sep = '')
+cat("Low Confidence coverage: ",low_conf_percent,'%', sep = '')
+
+#plot the Trait in SDM area
+#TraitSDM <- raster::mask(trait_rast, mask = sdm_mask)
+#then plot the traitSDM using mapPNG function
